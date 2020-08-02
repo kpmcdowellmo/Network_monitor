493f871d55a3b46d6bcc802e4155db1f0fd1ee8a
Keven McDowell <kpmcdowellmo@gmail.com>
2020-08-02 12:14:02 -0500

    rename include and stop trying to do crazy pointer things

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
-#include <stdlib.h>
+#include "headers/interfaces.h"
+#include "headers/init.h"
 #include <stdio.h>
-#include <headers/init.h>
+#include <stdlib.h>
 
-int main (int argc, char** argv){
-    WINDOW* window = init_curses();
-    return 0;
+int main(int argc, char **argv) {
+  printf("Attempting to find interfaces...\n");
+  struct network_interfaces network_interfaces = find_interfaces();
+  for (int i = 0; i < network_interfaces.num_interfaces; ++i) {
+    printf("Interface Name: %s\n", network_interfaces.interface_names[i]);
+  }
+  free_interface_names(&network_interfaces);
+  return 0;
 }
