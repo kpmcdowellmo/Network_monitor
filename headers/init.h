764b74bc9021ddc1ab414254c1075837d51a902b
Keven McDowell <kpmcdowellmo@gmail.com>
2020-09-09 21:12:43 -0500

    formatting

--- headers/init.h
+++ headers/init.h
@@ -1,64 +1,89 @@
 #include <ncurses.h>
-struct interface_section {
-  WINDOW* window;
-  struct interface_border* border;
-  int startx, starty, height, width;
+#include <stdio.h>
+#include <stdlib.h>
+const int_fast8_t MAX_COLS = 4;
+
+enum {
+
 };
 
 struct interface_border {
-chtype top_right, bottom_right, top_left, bottom_left, right_side, left_side, top, bottom;
+  chtype top_right, bottom_right, top_left, bottom_left, right_side, left_side,
+      top, bottom;
 };
-
+struct interface_section {
+  WINDOW *window;
+  struct interface_border border;
+  int startx, starty, height, width;
+};
 
 WINDOW *init_curses() {
   WINDOW *window = initscr();
-  int c_break_init = cbreak(), 
-    keypad_init = keypad(stdscr, true);
+  int c_break_init = cbreak(), keypad_init = keypad(stdscr, true);
 
-  if(c_break_init != 0){
-      printf("Error initializing cbreak for ncurses.");      
-      exit(1);
+  if (c_break_init != 0) {
+    printf("Error initializing cbreak for ncurses.");
+    exit(1);
   }
 
-  if(keypad_init != 0){
-      printf("Error initializing keypad for ncurses.");      
-      exit(1);
+  if (keypad_init != 0) {
+    printf("Error initializing keypad for ncurses.");
+    exit(1);
   }
 
   return window;
 }
 
-struct interface_section* init_section(WINDOW* window, int startx, int starty, int width, int height){
-  struct interface_section* new_section;
-  new_section->height = height;
-  new_section->width = width;
-  new_section->startx = startx;
-  new_section->starty = starty;
-  new_section->window = window;
+struct interface_section init_section(WINDOW *window, int startx, int starty,
+                                      int width, int height) {
+  struct interface_section new_section;
+  new_section.height = height;
+  new_section.width = width;
+  new_section.startx = startx;
+  new_section.starty = starty;
+  new_section.window = window;
   return new_section;
 }
 
-struct interface_border* init_border(chtype corner, chtype side, chtype top_bottom){
-  struct interface_border* new_border;
-  new_border->top_left = corner;
-  new_border->top_right = corner;
-  new_border->bottom_left = corner;
-  new_border->bottom_right = corner;
-  new_border->left_side = side;
-  new_border->right_side = side;
-  new_border->top = top_bottom;
-  new_border->bottom = top_bottom; 
+struct interface_border init_border(chtype corner, chtype side,
+                                    chtype top_bottom) {
+  struct interface_border new_border;
+  new_border.top_left = corner;
+  new_border.top_right = corner;
+  new_border.bottom_left = corner;
+  new_border.bottom_right = corner;
+  new_border.left_side = side;
+  new_border.right_side = side;
+  new_border.top = top_bottom;
+  new_border.bottom = top_bottom;
   return new_border;
 }
 
-struct interface_section* build_interface_section(WINDOW* main_window, int win_id, int width, int height){
-  int starty = height * win_id, startx = width * win_id;
-  WINDOW* new_win = newwin(height, width, starty, startx);
+struct interface_section build_interface_section(WINDOW *main_window,
+                                                 int win_id, int width,
+                                                 int height, int row) {
+  int starty = height * row, startx = width * win_id;
+  WINDOW *new_win = newwin(height, width, starty, startx);
+  box(new_win, 0, 0);
+  wrefresh(new_win);
 
-  struct interface_section* new_section = init_section(new_win, startx, startx, width, height);
+  struct interface_section new_section =
+      init_section(new_win, startx, starty, width, height);
+  struct interface_border new_border = init_border('+', '|', '-');
+  new_section.border = new_border;
   return new_section;
 }
 
-void create_box(struct interface_section* section){
- 
+void create_box(struct interface_section *section) {
+  int x = section->startx, y = section->starty, width = section->width,
+      height = section->height;
+
+  mvaddch(y, x, section->border.top_left);
+  mvaddch(y, x + width, section->border.top_right);
+  mvaddch(y + height, x + width, section->border.bottom_right);
+  mvaddch(y + height, x, section->border.bottom_left);
+  mvhline(y, x + 1, section->border.top, width - 1);
+  mvhline(y + height, x + 1, section->border.bottom, width - 1);
+  mvvline(y + 1, x, section->border.left_side, height - 1);
+  mvvline(y + 1, x + width, section->border.right_side, height - 1);
 }
--- headers/interface.h
+++ headers/interface.h
@@ -1,11 +1,12 @@
+#ifndef INTERFACE_H_
+#define INTERFACE_H_
+
 #include <dirent.h>
 #include <net/if.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
-
-const int8_t INITIAL_SIZE = 1;
 
 struct network_interfaces {
   char **interface_names;
@@ -17,3 +18,5 @@ void add_interface_name(struct network_interfaces *, const char *);
 void free_interface_names(struct network_interfaces *);
 void expand_interface_names(struct network_interfaces *);
 struct network_interfaces find_interfaces();
+
+#endif
